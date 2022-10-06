73cc226cf2d43f8dd5b7a573ea12635c4bfb89ca
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-10-06 22:47:02 +0300

    База №1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './js/kay.js';
+import './js/Alexandr';
+import './js/AnnaSobova';
+import './js/darinacat87';
+import './js/Ghozzty';
+import './js/LenaK513';
+import './js/MykolaPom';
+import './js/Natali2721';
+import './js/NataliaDavydenko';
+import './js/voprim';
+// Ідея лайно, але для початку згодиться, я думаю=)
---
+++ src/js/kay.js
@@ -0,0 +1,2 @@
+const KEY = `5d44c33850258a6ea050e461474b1468`;
+console.log(`ПРАЦЮЄ`);
