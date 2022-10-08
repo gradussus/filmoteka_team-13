885ead8c803e1237948dae5e6326e7cc86085fa6
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-10-08 10:25:03 +0300

    Update Natali2721.js

--- src/js/Natali2721.js
+++ src/js/Natali2721.js
@@ -1,9 +1,19 @@
 import { refs } from './refs';
 
-window.addEventListener('click', () =>
-  refs.backdrop.classList.add('is-hidden')
-);
+//window.addEventListener('click', () =>
+//  refs.backdrop.classList.add('is-hidden')
+//);
 
-function backdropHidden() {
-  refs.backdrop.classList.add('is-hidden');
+//function backdropHidden() {
+//  refs.backdrop.classList.add('is-hidden');
+//}
+
+document.addEventListener('keydown', onEscapeClick);
+
+function onEscapeClick(event) {
+  console.log('esc');
+  if (event.code == 'Escape') {
+    refs.backdrop.classList.add('is-hidden');
+    document.removeEventListener('keydown', onEscapeClick);
+  }
 }
