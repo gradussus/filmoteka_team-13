67e721b8df582f3788e3fb97d6ee1e094717fdf2
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-10-08 14:33:01 +0300

    render and some lattle fix

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,9 @@ const refs = {
   backdrop: document.querySelector('.backdrop'),
   teamLink: document.querySelector('.js-team'),
   spinerBackdrop: document.querySelector('.spiner__backdrop'),
+  filmsListRef: document.querySelector('.films'),
+  gallery: document.querySelector('.gallery__set'),
+  container: document.getElementById('pagination'),
 };
 
 export { refs };
