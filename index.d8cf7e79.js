c5e0431e4f4316b05d09fc73daee4bef9fb85192
gradussus <103496948+gradussus@users.noreply.github.com>
2022-10-08 07:28:31 +0000

    Deploying to gh-pages from @ gradussus/filmoteka_team-13@b31e3fde36ef089a74c258cdcd2a3c064776a285 🚀

---
+++ index.d8cf7e79.js
@@ -0,0 +1,2 @@
+const e={backdrop:document.querySelector(".backdrop"),teamLink:document.querySelector(".js-team")};document.addEventListener("keydown",(function o(c){console.log("esc"),"Escape"==c.code&&(e.backdrop.classList.add("is-hidden"),document.removeEventListener("keydown",o))}));document.querySelector(".films");
+//# sourceMappingURL=index.d8cf7e79.js.map
