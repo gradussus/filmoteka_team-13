dafd93dea2fe06a5edab9b5818dd6eae7ef4e5a0
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-10-08 12:05:09 +0300

    Update spiner.js

--- src/js/spiner.js
+++ src/js/spiner.js
@@ -1,8 +1,6 @@
 import { refs } from './refs';
 document.addEventListener('click', onSpinerBackdropClick);
 function onSpinerBackdropClick(event) {
-  //console.log('event');
-  //console.log(event);
   refs.spinerBackdrop.classList.add('is-hidden');
   document.removeEventListener('click', onSpinerBackdropClick);
 }
