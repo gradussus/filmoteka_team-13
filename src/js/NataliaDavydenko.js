06a29990b9b9b415c7a8634918db706eeb535930
Natalia <davydenko.n@ukr.net>
2022-10-09 17:52:40 +0300

    changed modal close function

--- src/js/NataliaDavydenko.js
+++ src/js/NataliaDavydenko.js
@@ -7,21 +7,48 @@ const refs = {
 refs.openFooterModalBtn.addEventListener('click', e => {
   e.preventDefault();
   refs.modal.classList.remove('is-hidden');
+  document.addEventListener('keydown', onEscapeClick);
+  document.addEventListener('click', onBackdropClick);
 });
-
 refs.closeBtn.addEventListener('click', e => {
   e.preventDefault();
   refs.modal.classList.add('is-hidden');
+  document.removeEventListener('keydown', onEscapeClick);
+  document.removeEventListener('click', onBackdropClick);
 });
-
-document.addEventListener('keydown', e => {
-  if (e.key === 'Escape') {
+function onEscapeClick(event) {
+  if (event.code === 'Escape') {
     refs.modal.classList.add('is-hidden');
+    document.removeEventListener('keydown', onEscapeClick);
+    document.removeEventListener('click', onBackdropClick);
   }
-})
-
-document.addEventListener('click', e => {
-  if (e.target === refs.modal) {
+}
+function onBackdropClick(event) {
+  if (event.target === refs.modal) {
     refs.modal.classList.add('is-hidden');
+    document.removeEventListener('click', onBackdropClick);
+    document.removeEventListener('keydown', onEscapeClick);
   }
-})
+}
+
+// refs.openFooterModalBtn.addEventListener('click', e => {
+//   e.preventDefault();
+//   refs.modal.classList.remove('is-hidden');
+// });
+
+// refs.closeBtn.addEventListener('click', e => {
+//   e.preventDefault();
+//   refs.modal.classList.add('is-hidden');
+// });
+
+// document.addEventListener('keydown', e => {
+//   if (e.key === 'Escape') {
+//     refs.modal.classList.add('is-hidden');
+//   }
+// })
+
+// document.addEventListener('click', e => {
+//   if (e.target === refs.modal) {
+//     refs.modal.classList.add('is-hidden');
+//   }
+// })
