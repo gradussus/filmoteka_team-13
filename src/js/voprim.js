114d5122b8c71833e892a190011943a37388d8bb
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-10-10 23:30:51 +0300

    fix-genres

--- src/js/voprim.js
+++ src/js/voprim.js
@@ -12,22 +12,29 @@ export function renderFilmsMarkup(films) {
                 <div class="films__description">
                   <p class="films__title">${title}</p>
                   <div class="films__meta">
-                    <span class="films__genres">${getGenres(genre_ids)}</span>
+                    <span class="films__genres">${getGenres(
+                      genre_ids,
+                      3
+                    )}</span>
                     <span class="films__sep">|</span>
-                    <span class="films__data">${(
-                      release_date || 'No info'
-                    ).slice(0, 4)}</span>
+                    <span class="films__data">${
+                      release_date.slice(0, 4) || 'No info'
+                    }</span>
                   </div>
                 </div>
             </li>`;
     })
     .forEach(c => refs.gallery.insertAdjacentHTML('beforeend', c));
 }
-export function getGenres(genre_ids) {
+export function getGenres(genre_ids, maxGenresShown) {
   const genres = JSON.parse(localStorage.getItem('genres'));
   const genresArr = [];
 
-  for (let genreIndex = 0; genreIndex < genre_ids.length; genreIndex++) {
+  for (
+    let genreIndex = 0;
+    genreIndex < maxGenresShown && genreIndex < genre_ids.length;
+    genreIndex++
+  ) {
     for (const value of genres) {
       if (genre_ids[genreIndex] === value.id) {
         genresArr.push(value.name);
@@ -35,10 +42,35 @@ export function getGenres(genre_ids) {
     }
   }
   if (genresArr.length === 0) {
-    return 'No info';
+    return 'No genres info';
   }
-  // console.log(genresArr);
+  if (genre_ids.length > maxGenresShown) {
+    genresArr.push('Other');
+  }
+
   return genresArr.join(', ');
+}
+
+export function renderFilmsLibrary(films) {
+  films
+    .map(({ poster_path, genre_ids, title, release_date, id }) => {
+      return `<li class="films__item" data-id=${id}>
+                <div class="films__img">
+                    <img src=https://image.tmdb.org/t/p/original${poster_path} alt="${title}" loading="lazy">
+                </div>
+                <div class="films__description">
+                  <p class="films__title">${title}</p>
+                  <div class="films__meta">
+                    <span class="films__genres">${getGenres(genre_ids)}</span>
+                    <span class="films__sep">|</span>
+                    <span class="films__data">${
+                      release_date.slice(0, 4) || 'No year info'
+                    }</span>
+                  </div>
+                </div>
+            </li>`;
+    })
+    .forEach(c => refs.libGallery.insertAdjacentHTML('beforeend', c));
 }
 
 // функция отрисовки жанров
@@ -73,24 +105,3 @@ export function getGenres(genre_ids) {
 //   }
 // }
 // lib-gallery-render-func
-export function renderFilmsLibrary(films) {
-  films
-    .map(({ poster_path, genre_ids, title, release_date, id }) => {
-      return `<li class="films__item" data-id=${id}>
-                <div class="films__img">
-                    <img src=https://image.tmdb.org/t/p/original${poster_path} alt="${title}" loading="lazy">
-                </div>
-                <div class="films__description">
-                  <p class="films__title">${title}</p>
-                  <div class="films__meta">
-                    <span class="films__genres">${getGenres(genre_ids)}</span>
-                    <span class="films__sep">|</span>
-                    <span class="films__data">${(
-                      release_date || 'No info'
-                    ).slice(0, 4)}</span>
-                  </div>
-                </div>
-            </li>`;
-    })
-    .forEach(c => refs.libGallery.insertAdjacentHTML('beforeend', c));
-}
