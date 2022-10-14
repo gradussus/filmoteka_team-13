8c9fb4b3b46bb07d88549d6fc0cd61b9e28a6090
Fedor <ghozztt@gmail.com>
2022-10-14 20:22:17 +0300

    Update voprim.js

--- src/js/voprim.js
+++ src/js/voprim.js
@@ -62,7 +62,7 @@ export function renderFilmsLibrary(films) {
         const poster = poster_path
           ? `https://image.tmdb.org/t/p/w500${poster_path}`
           : `https://image.tmdb.org/t/p/w500/yEvumAoCB9Z7o9dAzjxrjcwo2FQ.jpg`;
-        return `<li class="films__item" data-id=${id || `No ID`}>
+        return `<li class="films__item" id=${id || `No ID`}>
                 <div class="films__img">
                     <img src=https://image.tmdb.org/t/p/original${poster} alt="${
           title || 'No title'
