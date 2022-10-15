07a7a4a3a71790b84d4b01efc115a9d5b73f2856
Vitalii Shevchenko <shevchenkovitaliy20@gmail.com>
2022-10-15 18:33:26 +0300

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -9,17 +9,21 @@ function saveTheme() {
   localStorage.setItem('dark-theme', JSON.stringify(refs.checkBox.checked));
   if (refs.checkBox.checked) {
     refs.main.style.backgroundColor = '#272e34';
+    refs.modalForOneMovie.style.backgroundColor = '#272e34';
   }
   if (!refs.checkBox.checked) {
     refs.main.style.backgroundColor = '#fff';
+    refs.modalForOneMovie.style.backgroundColor = '#fff';
   }
 }
 export function loadTheme() {
   refs.checkBox.checked = JSON.parse(localStorage.getItem('dark-theme'));
   if (refs.checkBox.checked) {
     refs.main.style.backgroundColor = '#272e34';
+    refs.modalForOneMovie.style.backgroundColor = '#272e34';
   }
   if (!refs.checkBox.checked) {
     refs.main.style.backgroundColor = '#fff';
+    refs.modalForOneMovie.style.backgroundColor = '#fff';
   }
 }
